413e5b09a6dabbbe12f9047a4c9d3ce3f2863007
lukyncze <lukas.lukyn.sukenik@gmail.com>
2019-05-02 15:30:35 +0200

    1.3

--- js/map.js
+++ js/map.js
@@ -2,7 +2,9 @@ $(function(){
     $('h2').text('Městské části New Yorku');
     $('.casti').attr({'style':'fill:rgb(90,90,90);stroke:black'});
 
-    $('.casti').on('click',function(){
+    $('.casti').on('mouseover',function(){
+        $('.casti').attr({'style':'fill:rgb(90,90,90);stroke:black'});
+        $(this).attr({'style':'fill:rgb(255,20,20);stroke:black'});
         $('#detail h2').text($(this).data('nazev'));
         $('#detail p').text($(this).data('popis'));
         $('#detail img').attr('src',$(this).data('img')).attr('alt',$(this).data('nazev'));
