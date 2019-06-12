41d0bcb42a07a5cb3d1d00caf47163a7d770c4e3
lukyncze <lukas.lukyn.sukenik@gmail.com>
2019-06-12 22:27:33 +0200

    1.6

---
+++ js/security.js
@@ -0,0 +1,8 @@
+function check(form){
+    if(form.user.value == "admin" && form.pass.value == "admin123"){
+        window.open('../client/index.html');
+    }    
+    else{
+        alert('Chybně zadané heslo.');
+    }
+}
